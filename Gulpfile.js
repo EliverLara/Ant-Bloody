0ec44e70a02b2a013d8f6756d7760e3ae18136e7
EliverLara <eliverlara@gmail.com>
2017-10-13 22:59:52 -0500

    v1.0.1

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,10 +6,22 @@ gulp.task('styles', function() {
     gulp.src('gtk-3.20/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./gtk-3.20/'))
-        .pipe(exec(' gsettings set org.gnome.desktop.interface gtk-theme "Ant"'))
+        .pipe(exec(' gsettings set org.gnome.desktop.interface gtk-theme "Bloody"'))
+});
+
+gulp.task('shell-style', function() {
+    gulp.src('gnome-shell/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./gnome-shell/'))
+        .pipe(exec('gsettings set org.gnome.shell.extensions.user-theme name "Bloody"'))
 });
 
 //Watch task
 gulp.task('default',function() {
     gulp.watch('gtk-3.20/**/*.scss',['styles']);
 });
+
+gulp.task('shell',function() {
+    gulp.watch('gnome-shell/*.scss',['shell-style']);
+});
+
